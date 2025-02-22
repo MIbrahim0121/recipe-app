2f1342f9f6c9311636bebf26fb7e65ec768c2909
MUHAMMAD IBRAHIM <hfzmibrahim295@gmail.com>
2025-02-22 12:10:19 +0500

    my changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Recipecard from "./components/Recipecard";
 import Searchbar from "./components/Searchbar";
 
 const api = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
 const App = () => {
   const [isloading, setIsloading] = useState(false);
   const [query, setQuery] = useState("");
@@ -15,33 +16,36 @@ const App = () => {
     const data = await res.json();
     console.log(data);
 
-    setRecipes(data.meals);
+    setRecipes(data.meals || []); // Ensure recipes is an array even if no data is found
     setIsloading(false);
   };
+
   useEffect(() => {
     searchRecipes();
   }, []);
-  const handlesubmit=event=>{
-    event.preventDefault()
-    searchRecipes()
-  }
+
+  const handlesubmit = (event) => {
+    event.preventDefault();
+    searchRecipes();
+  };
 
   return (
     <div className="container">
       <h1>Recipe App</h1>
       <Searchbar
-      handlesubmit={handlesubmit}
-      value={query}
-
-      onChange={(event)=>setQuery(event.target.value)}
-      isloading={isloading}
+        handlesubmit={handlesubmit}
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+        isloading={isloading}
       />
       <div className="recipes">
-        {recipes
-        ? recipes.map((recipe)=>(
-          <Recipecard key={recipe.idMeal } recipe={recipe} />
-        )):"No Recipes!"}
-
+        {recipes && recipes.length > 1 ? (
+          recipes.slice(1).map((recipe) => ( // Remove the first recipe using slice(1)
+            <Recipecard key={recipe.idMeal} recipe={recipe} />
+          ))
+        ) : (
+          "No Recipes!"
+        )}
       </div>
     </div>
   );
